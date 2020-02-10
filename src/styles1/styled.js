70466712fcac2eeafd362af4b2f8bf91cfb78076
Stanley Johnson <stanley.johnson@sprinklr.com>
2020-02-10 13:14:17 +0530

    changing style impl

---
+++ src/styles1/styled.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import _styled from '@emotion/styled';
+
+import {ThemeContext} from './theme-provider.js';
+
+export const styled = (type, getStyles) => {
+  return React.forwardRef((props, ref) => (
+    <ThemeContext.Consumer>
+      {theme => {
+        const StyledComponent = _styled[type](getStyles);
+        return <StyledComponent ref={ref} {...props} $theme={theme} />;
+      }}
+    </ThemeContext.Consumer>
+  ));
+};
+
+export const withStyle = styled;
+
+export const useStyletron = () => {
+  const theme = React.useContext(ThemeContext);
+  const css = p => p;
+  return [css, theme];
+};
+
+export const createThemedStyled = () => styled;
+export const createThemedWithStyle = () => withStyle;
+export const createThemedUseStyletron = () => useStyletron;
